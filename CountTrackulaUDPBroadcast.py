d9bc302ced61d20751f2986c2a3549c47d1e0e7f
Kimon Kremizas <34919242+kkremizas@users.noreply.github.com>
2020-11-28 11:47:43 +0100

    Update CountTrackulaUDPBroadcast.py
    Experiment 1 with get_orientation and all imu sensors enabled

--- CountTrackulaUDPBroadcast.py
+++ CountTrackulaUDPBroadcast.py
@@ -1,21 +1,44 @@
-BROADCAST_TO_PORT = 7000
+from datetime import datetime
 import time
 from socket import *
-from datetime import datetime
-import random
+from sense_hat import SenseHat
 
+BROADCAST_TO_PORT = 7000
 s = socket(AF_INET, SOCK_DGRAM)
-#s.bind(('', 14593))     # (ip, port)
-# no explicit bind: will bind to default IP + random port
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+sense = SenseHat()
+sense.set_imu_config(True, True, True)
+red = (255, 0, 0)
+green = (0, 255, 0)
+counter=0
+sense.clear()
+
+
+sense.show_message("WAIT", text_colour=[255, 0, 0])
+time.sleep(15)
+sense.show_message("GO", text_colour=[255, 0, 0])
+o = sense.get_orientation()
+resting_yaw = round(o["yaw"],3)
+print("Resting yaw: {0}".format(resting_yaw))
+    
 while True:
-    now = datetime.now()
-    strftime = now.strftime('%Y-%m-%d %H:%M:%S.%f')
-    strftimeString = str(strftime[:-4])
-    value = random.randint(0,1)
-    isEntranceString = str(value)
-        # THis will add the datetimenow and the IsEntrance bit/bool value
-    data =strftimeString+","+isEntranceString
-    s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
-    print(data)
-    time.sleep(2)
+    o = sense.get_orientation()
+    yaw = round(o["yaw"],3)
+    print("yaw: {0}".format(yaw))
+    if yaw>resting_yaw+5.5 and yaw<resting_yaw+9.5:
+        if counter != 1:
+            print("-------------Door opened. PUT BROADCAST HERE")
+            now = datetime.now()
+            strftime = now.strftime('%Y-%m-%d %H:%M:%S.%f')
+            strftimeString = str(strftime[:-4])
+            isEntranceString = str(1)
+            data =strftimeString+","+isEntranceString
+            s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
+            sense.clear(red)
+            counter += 1
+        else:
+            print("-------------Door closed")
+            sense.clear(green)
+            counter = 0
+    time.sleep(0.10)

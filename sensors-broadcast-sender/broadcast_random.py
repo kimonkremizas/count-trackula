4f536dce1691d025fc6a59cb065e4c4bdc2ac5a7
Kimon Kremizas <k.kremizas@gmail.com>
2021-04-15 13:41:16 +0200

    Moved sensors-broadcast-sender files into subdirectory

---
+++ sensors-broadcast-sender/broadcast_random.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+import time
+from socket import *
+import random
+
+BROADCAST_TO_PORT = 57054
+s = socket(AF_INET, SOCK_DGRAM)
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+counter=0
+    
+while True:
+    now = datetime.now()
+    strftime = now.strftime('%Y-%m-%d %H:%M:%S.%f')
+    strftimeString = str(strftime[:-4])
+    print(strftimeString)
+    value = random.randint(0,1)
+    isEntranceString = str(value)
+    data =strftimeString+","+isEntranceString
+    s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
+    time.sleep(30)
